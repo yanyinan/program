ed29e699e66d05c5c3d721f87cb31730df609f4d
Nanzhou <2648179906@qq.com>
2023-08-12 17:35:07 +0800

    2023年08月12日

--- 练习/JAVA练习/codeproject/Java_Exersice_nine/src/Main.java
+++ 练习/JAVA练习/codeproject/Java_Exersice_nine/src/Main.java
@@ -1,5 +1,23 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+// 调用showSalary方法，给可变参数传入散列值
+        showSalary("开发部", 1987, 2001, 2005);
+// 调用showSalary方法，给可变参数传入数组
+        showSalary("测试部", new double[]{2009, 1888});
+    }
+
+    // salary定义为可变参数
+    public static void showSalary(String dname, double... salary) {
+        double sum = 0;
+        if (salary != null && salary.length > 0) {
+            for (int i = 0; i < salary.length; i++) {
+                sum += salary[i];
+            }
+        }
+        System.out.printf("%s部门的平均薪资是%.2f", dname, sum /
+                salary.length);
     }
 }
