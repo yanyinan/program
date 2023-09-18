abff959a3bc015713496e5dd56af804d2bbca2f6
Nanzhou <2648179906@qq.com>
2023-09-18 17:00:06 +0800

    2023年09月18日

--- 练习/JAVA练习/codeproject/Test_the_project/src/thread/Demo01.java
+++ 练习/JAVA练习/codeproject/Test_the_project/src/thread/Demo01.java
@@ -11,9 +11,15 @@ public class Demo01 {
         Thread t1 = new Thread(test, "线程1");
         Thread t2 = new Thread(test, "线程2");
         Thread t3 = new Thread(test, "线程3");
+        Thread t4 = new Thread(test, "线程4");
+        Thread t5 = new Thread(test, "线程5");
+        Thread t6 = new Thread(test, "线程6");
         t1.start();
         t2.start();
         t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
     }
 }
 
@@ -29,8 +35,10 @@ class TicketSell implements Runnable {
 
     private  void takeTicket(int ticket) {
         while (true) {
+
             //同步代码块锁
             synchronized (this) {
+//                if (ticket>0){
                 try {
                     //模拟出票
                     Thread.sleep(100);
@@ -40,7 +48,7 @@ class TicketSell implements Runnable {
                 //获取线程名
                 String name = Thread.currentThread().getName();
                 System.out.println(name + " " + ticket--);
-            }
-        }
+//            }
+        }}
     }
 }
--- 练习/JAVA练习/codeproject/Test_the_project/src/thread/Demo02.java
+++ 练习/JAVA练习/codeproject/Test_the_project/src/thread/Demo02.java
@@ -9,57 +9,51 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Demo02 {
     public static void main(String[] args) {
-
+        AtomicInteger num = new AtomicInteger();
         Runnable a = new Runnable() {
-            int num = 1;
-            int i = 10;
+
+            int i = 0;
 
             @Override
             public void run() {
-
-                while (i<100) {
+                while (i< 100) {
+                    i++;
                     synchronized (this) {
-                        num++;
-                        i++;
-//                        try {
-//                            Thread.sleep(100);
-//                        } catch (InterruptedException e) {
-//                            throw new RuntimeException(e);
-//                        }
-                        System.out.println(Thread.currentThread().getName() + "  " + i);
+                        num.getAndIncrement();
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+//                        System.out.println(Thread.currentThread().getName() + "  " + num);
                     }
                 }
             }
         };
-//        Runnable d = new Runnable() {
-//            @Override
-//            public void run() {
-//                for (int i = 0; i < 100; i++) {
-//                    synchronized (this) {
+        Runnable d = new Runnable() {
+
+            int i = 0;
+
+            @Override
+            public void run() {
+                while (i< 100) {
+                    i++;
+                    synchronized (this) {
+                        num.getAndDecrement();
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
 //                        System.out.println(Thread.currentThread().getName() + "  " + num);
-//                    }
-//                }
-//            }
-//        };
-        Thread t1 = new Thread(a,"累加1");
-        Thread t2 = new Thread(a,"累加2");
-        Thread t3 = new Thread(a,"累加3");
-        Thread t4 = new Thread(a,"累加4");
-        Thread t5 = new Thread(a,"累加5");
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
-//        Thread d1 = new Thread(a,"累减1");
-//        Thread d2 = new Thread(a,"累减2");
-//        Thread d3 = new Thread(a,"累减3");
-//        Thread d4 = new Thread(a,"累减4");
-//        Thread d5 = new Thread(a,"累减5");
-//        d1.start();
-//        d2.start();
-//        d3.start();
-//        d4.start();
-//        d5.start();
+                    }
+                }
+            }
+        };
+        for (int i = 0; i < 5; i++) {
+            new Thread(a,"累加"+i).start();
+            new Thread(d,"累减"+i).start();
+        }
+        System.out.println(num);
     }
 }
