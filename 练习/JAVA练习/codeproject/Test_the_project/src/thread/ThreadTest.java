36448166cc6ac69307e13cb0000c65749eda6f85
Nanzhou <2648179906@qq.com>
2023-09-19 22:20:21 +0800

    2023年09月19日

--- 练习/JAVA练习/codeproject/Test_the_project/src/thread/ThreadTest.java
+++ 练习/JAVA练习/codeproject/Test_the_project/src/thread/ThreadTest.java
@@ -1,8 +1,6 @@
 package thread;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.*;
 
 /**
  * @title:
@@ -11,44 +9,82 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ThreadTest {
     static int i = 1;
+
     public static void main(String[] args) {
         Object jude = new Object();
 
-        Thread t1 = new Thread() {
-            @Override
-            public void run() {
-               while (i<11){
-                    if (i % 2 == 0) {
-                        try {
-                            t1.wait();
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(2, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        try {
+            poolExecutor.execute(() -> {
+                while (i < 10) {
+                    synchronized (jude) {
+                        if (i % 2 == 0) {
+                            try {
+                                jude.wait();
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
                         }
+                        System.out.println(Thread.currentThread().getName() + " : " + i++);
+                        jude.notifyAll();
                     }
-                    System.out.println(Thread.currentThread().getName() + " : " + i);
-                    t2.notify();
                 }
-            }
-        };
+            });
+            poolExecutor.execute(() -> {
+                while (i < 11) {
+                    synchronized (jude) {
+                        if (i % 2 != 0) {
+                            try {
+                                jude.wait();
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        System.out.println(Thread.currentThread().getName() + " : " + i++);
+                        jude.notifyAll();
+                    }
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            // 关闭线程池，不关闭的话，程序不会停止，一直处于Waiting状态
+            poolExecutor.shutdown();
+        }
+//
+//        Thread t1 = new Thread(() -> {
+//            while (i < 10) {
+//                synchronized (jude) {
+//                    if (i % 2 == 0) {
+//                        try {
+//                            jude.wait();
+//                        } catch (InterruptedException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    }
+//                    System.out.println(Thread.currentThread().getName() + " : " + i++);
+//                    jude.notifyAll();
+//                }
+//            }
+//        });
 
-        Thread t2 = new Thread() {
-            @Override
-            public void run() {
-                while (i<11){
-                    if (i % 2 != 0) {
-                        try {
-                            t2.wait();
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    System.out.println(Thread.currentThread().getName() + " : " + i);
-                    t1.notify();
-                }
-            }
-        };
-        new Thread(t1,"奇数").start();
-        new Thread(t2,"偶数").start();
+//        Thread t2 = new Thread(() -> {
+//            while (i < 11) {
+//                synchronized (jude) {
+//                    if (i % 2 != 0) {
+//                        try {
+//                            jude.wait();
+//                        } catch (InterruptedException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    }
+//                    System.out.println(Thread.currentThread().getName() + " : " + i++);
+//                    jude.notifyAll();
+//                }
+//            }
+//        });
+//        new Thread(t1, "奇数").start();
+//        new Thread(t2, "偶数").start();
     }
 }
 
