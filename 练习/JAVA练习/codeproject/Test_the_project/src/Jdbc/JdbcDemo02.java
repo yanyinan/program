8fa51b7801fe9f92285a50c626a1db8ba6a5ebcc
Nanzhou <2648179906@qq.com>
2023-09-21 22:06:51 +0800

    2023年09月21日

--- 练习/JAVA练习/codeproject/Test_the_project/src/Jdbc/JdbcDemo02.java
+++ 练习/JAVA练习/codeproject/Test_the_project/src/Jdbc/JdbcDemo02.java
@@ -1,11 +1,7 @@
 package Jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
-import static java.lang.Class.forName;
 
 /**
  * @title:
@@ -13,33 +9,94 @@ import static java.lang.Class.forName;
  * @date:
  */
 public class JdbcDemo02 {
-    public static void main(String[] args) {
 
-        opar();
+    public static void main(String[] args) {
+        del();
     }
 
-    private static void opar() {
+    /**
+     * 建立连接
+     * @return Connection 返回数据库连接
+     */
+    private static Connection con() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
             String name = "root";
             String pass = "";
             String url = "jdbc:mysql://localhost:3306/company";
-            try (Connection oparConnection = DriverManager.getConnection(url, name, pass);
-                 Statement oparStatement = oparConnection.createStatement()) {
-                int product_id = 12;
-                String product_name = "苹果";
-                String category = "水果";
-                int price = 121;
-                //Sql 语句
-                String opar  = "insert into products(product_id, product_name, category, price) values (%d,%s,%s,%d)".formatted(product_id,product_name,category,price);
-                oparStatement.execute(opar);
-                int upRow = oparStatement.getUpdateCount();
-                System.out.println(upRow);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, name, pass);
+            return connection;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    private static void del(String ) {
+        try (Statement statement = con().createStatement();){
+            String sql = "delete from products where product_id = 2;";
+            if (statement.executeUpdate(sql) >0){
+                System.out.println("成功");
+            }else {
+                System.out.println("未添加成功");
+            }
+            con().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void update() {
+        try (Statement statement = con().createStatement();){
+            String sql = "update products set price = 20 where product_id = 5;";
+            statement.execute(sql);
+            con().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void select() {
+        try (Statement statement = con().createStatement();){
+            String sql = "select * from products";
+            boolean execute = statement.execute(sql);
+            if (execute) {
+                System.out.println("执行成功");
+
+            }
+            ResultSet resultSet = statement.getResultSet();
+            System.out.println(resultSet);
+            while (resultSet.next()) {
+                for (int i = 1; i < 5; i++) {
+                    Object object = resultSet.getObject(i);
+                    System.out.print(object+"\t");
+                }
+                System.out.println();
+            }
+            resultSet.close();
+            con().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void opar() {
+        try (Statement statement = con().createStatement();){
+            int product_id = 12;
+            String product_name = "121";
+            String category = "232";
+            int price = 121;
+            //Sql 语句
+            String opar  = "insert into products(product_id, product_name, category, price) values (%d,%s,%s,%d)".formatted(product_id,product_name,category,price);
+            if (statement.executeUpdate(opar) >0){
+                System.out.println("成功");
+            }else {
+                System.out.println("未添加成功");
+            }
+            con().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
