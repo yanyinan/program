4284ac7c0f7ccf8f15d640a3157a2fa090e4cdb1
Nanzhou <2648179906@qq.com>
2023-10-15 20:37:32 +0800

    2023年10月15日

---
+++ 练习/Java_Servlet/src/xyz/nanzhou/fw/servlet/ServletDemo003.java
@@ -0,0 +1,38 @@
+package xyz.nanzhou.fw.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author: Nanzhou
+ * @version: v0.0.1
+ * @date: 2023 2023/10/14 13:55
+ */
+@WebServlet("/parameter")
+public class ServletDemo003  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("ParameterServlet 接收到请求");
+
+        // 设置请求编码
+        req.setCharacterEncoding("utf-8");
+
+        // 获取请求参数
+        String username = req.getParameter("username");
+        String[] hobbies = req.getParameterValues("hobby");
+
+        System.out.println("username : " + username);
+        for (String hobby : hobbies) {
+            System.out.println(hobby);
+        }
+    }
+}
